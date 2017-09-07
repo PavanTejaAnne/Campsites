cf5cccae79853eac3feeabe88d37ad0fa31004cf
PavanTejaAnne <pavanteja@Pavans-MacBook-Pro.local>
2017-09-06 18:03:11 -0700

    Comments designed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express"),
     mongoose = require("mongoose"),
     Campground = require("./models/campgrounds"),
     seedDB = require("./seeds"),
+    Comment = require("./models/comments"),
     app = express()
 
 seedDB();
@@ -63,6 +64,25 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
         }else{
             console.log(campground);
             res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req,res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+campground._id);
+                }
+            });
         }
     });
 });
